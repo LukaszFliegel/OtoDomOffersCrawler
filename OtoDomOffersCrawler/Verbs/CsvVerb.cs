7c7b0247187b36633231ab3cf2b893430951579d
Lukasz Fliegel <Lukasz_Fliegel@epam.com>
2022-08-10 17:54:05 +0200

    Removed copied code :)

--- OtoDomOffersCrawler/Verbs/CsvVerb.cs
+++ OtoDomOffersCrawler/Verbs/CsvVerb.cs
@@ -16,19 +16,10 @@ namespace OtoDomOffersCrawler.Verbs
         [Option('r', "resources", Required = true, HelpText = "Resources url to parse")]
         public string OffersUrl { get; set; } = string.Empty;
 
-        //[Option('d', "darker", Required = false, HelpText = "Make hover colors darker")]
-        //public bool Darker { get; set; }
-
         [Option('i', "inputfilepath", Required = false, Default = "input.csv", HelpText = "path for the csv file to read previous reads from")]
         public string InputFilePath { get; set; } = "input.csv";
 
         [Option('o', "outputfilepath", Required = false, Default = "output.csv", HelpText = "path for the csv file to generate")]
         public string OutputFilePath { get; set; } = "output.csv";
-
-        //[Option('o', "openFile", Required = false, HelpText = "If true (which is default) generated file will be opened at the end", Default = true)]
-        //public bool OpenFile { get; set; }
-
-        //[Option('s', "strength", Required = false, HelpText = "Strength of making the hover color (darker or lighter depends on -d option). Takes values between 0 and 1.", Default = 0.5)]
-        //public double Strength { get; set; }
     }
 }
